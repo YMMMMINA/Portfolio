002e79ba544deb61d90d84d8581854a4494df16b
YMMMMINA <92845310+YMMMMINA@users.noreply.github.com>
2021-11-11 13:36:16 +0900

    21.11.10

---
+++ Cording/21.11.10/js/js.js
@@ -0,0 +1,20 @@
+new fullpage('#fullpage', {
+    sectionsColor: ['grey','#afafaf','green'],
+    verticalCentered: false,
+    scrollingSpeed: 1700,
+    scrollBar: true,
+    anchors: ['anchor1', 'anchor2', 'anchor3','anchor4'],
+    menu: '#menu'
+});
+
+
+
+
+
+
+// var myFullpage = new fullpage('#fullpage', {
+//     verticalCentered: true,
+//     anchors: ['page1', 'page2', 'page3'],
+//     menu : '#menu',
+//     sectionsColor:['grey','#afafaf','#efefef']
+// });
